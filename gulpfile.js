061bc10f50b93c260e4f1d6dd71d7456025d811b
Seth Whitaker <sdw3489@gmail.com>
2016-07-22 23:47:37 -0400

    Create a Gulp Build script with assemble

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var htmlmin = require('gulp-htmlmin');
+var extname = require('gulp-extname');
+var assemble = require('assemble');
+var app = assemble();
+
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
   	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
@@ -11,4 +16,48 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('assemble:watch', function () {
+  gulp.watch('./src/**/*.hbs', ['assemble']);
+});
+
+
+gulp.task('watch', ['sass:watch','assemble:watch']);
+
+gulp.task('default', ['sass','assemble']);
+
+//Load the assemble config
+gulp.task('load', function(cb) {
+  app.partials('src/partials/*.hbs');
+  app.layouts('src/layouts/*.hbs');
+  app.pages('src/pages/**/*.hbs');
+  app.create('docs');
+  app.docs('src/style-guide/**/*.hbs');
+  cb();
+});
+
+//Assemble Tasks
+app.task('pages', function() {
+  return app.toStream('pages')
+    .pipe(app.renderFile())
+    .pipe(htmlmin())
+    .pipe(extname())
+    .pipe(app.dest('www'));
+});
+app.task('docs', function() {
+  return app.toStream('docs')
+    .pipe(app.renderFile())
+    .pipe(htmlmin())
+    .pipe(extname())
+    .pipe(app.dest('www/style-guide/'));
+});
+
+gulp.task('assemble', ['load'], function() {
+  app.build(['pages','docs'], function(err) {
+    if (err) throw err;
+    console.log('done!');
+    // cb();
+  });
+
 });
